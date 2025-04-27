e4b8354640f1139c70db656c54da31809564d5e7
Rohit Singh Thakur <thakurrohitsingh04@gmail.com>
2025-04-27 11:22:40 +0530

    Add skill categories

---
+++ src/data/skills.js
@@ -0,0 +1,19 @@
+export const skillCategories = [
+    {
+      category: "Frontend",
+      skills: ["HTML", "CSS", "JavaScript", "React", "Tailwind CSS"]
+    },
+    {
+      category: "Backend",
+      skills: ["Node.js", "Express", "MongoDB", "REST API"]
+    },
+    {
+      category: "Tools",
+      skills: ["Git", "GitHub", "VS Code","Postman"]
+    },
+    {
+      category: "Other",
+      skills: ["Responsive Design"]
+    }
+  ];
+  
