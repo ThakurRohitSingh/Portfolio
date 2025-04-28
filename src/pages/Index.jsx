594c6200f5e97745d604ca35c2760101c4dd81ab
Rohit Singh Thakur <thakurrohitsingh04@gmail.com>
2025-04-28 11:39:35 +0530

    feat: add homepage layout

---
+++ src/pages/Index.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import { Hero } from '../components/sections/Hero';
+import { About } from '../components/sections/About';
+import { Projects } from '../components/sections/Projects';
+import { Contact } from '../components/sections/Contact';
+import { Navbar } from '../components/Navbar';
+import { Footer } from '../components/Footer';
+import { ThemeProvider } from '../components/ThemeProvider';
+
+const Index = () => {
+  return (
+    <ThemeProvider>
+      <Navbar />
+      <main>
+        <Hero />
+        <About />
+        <Projects />
+        <Contact />
+      </main>
+      <Footer />
+    </ThemeProvider>
+  );
+};
+
+export default Index;
