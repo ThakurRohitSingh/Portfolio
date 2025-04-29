2d657a7e56f6943a784290ee18ff91cb4f4ac62d
Rohit Singh Thakur <thakurrohitsingh04@gmail.com>
2025-04-29 10:15:41 +0530

    feat: add theme provider

--- src/components/ThemeProvider.jsx
+++ src/components/ThemeProvider.jsx
@@ -1,9 +1,39 @@
-import React from 'react'
+import React, { createContext, useContext, useEffect, useState } from "react";
 
-const ThemeProvider = () => {
+const ThemeProviderContext = createContext(undefined);
+
+export function ThemeProvider({ children, storageKey = "theme", ...props }) {
+  const [theme, setTheme] = useState(() => {
+    const storedTheme = localStorage.getItem(storageKey);
+    return storedTheme ? storedTheme : "light"; // default to light if nothing stored
+  });
+
+  useEffect(() => {
+    const root = window.document.documentElement;
+    root.classList.remove("light", "dark");
+
+    root.classList.add(theme); // just add 'light' or 'dark'
+  }, [theme]);
+
+  useEffect(() => {
+    localStorage.setItem(storageKey, theme); // save user's choice
+  }, [theme, storageKey]);
+
+  const value = { theme, setTheme };
+
   return (
-    <div>ThemeProvider</div>
-  )
+    <ThemeProviderContext.Provider value={value} {...props}>
+      {children}
+    </ThemeProviderContext.Provider>
+  );
 }
 
-export default ThemeProvider
+export const useTheme = () => {
+  const context = useContext(ThemeProviderContext);
+
+  if (context === undefined) {
+    throw new Error("useTheme must be used within a ThemeProvider");
+  }
+
+  return context;
+};
